51597627d820d31d13475eadc029c4e4b59f7faf
Rudhra6467 <117248592+Rudhra6467@users.noreply.github.com>
2026-09-03 07:25:16 -0400

    phase4: observation engine and persistence

--- research_db/persist/__init__.py
+++ research_db/persist/__init__.py
@@ -1,3 +1,15 @@
-from research_db.persist.store import FixtureStore, LiveQueryDenied, open_evidence_store, open_fixture_store
+from research_db.persist.store import (
+    FixtureStore,
+    LiveQueryDenied,
+    open_evidence_store,
+    open_fixture_store,
+    open_observation_store,
+)
 
-__all__ = ["FixtureStore", "LiveQueryDenied", "open_evidence_store", "open_fixture_store"]
+__all__ = [
+    "FixtureStore",
+    "LiveQueryDenied",
+    "open_evidence_store",
+    "open_fixture_store",
+    "open_observation_store",
+]
