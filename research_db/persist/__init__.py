f2e38a8540611bf7f2925bd13905cd4c552eff3b
Rudhra6467 <117248592+Rudhra6467@users.noreply.github.com>
2026-09-04 11:42:43 -0400

    phase9-10: analytical features and versioned regime definitions

--- research_db/persist/__init__.py
+++ research_db/persist/__init__.py
@@ -4,12 +4,16 @@ from research_db.persist import state as phase5_state
 from research_db.persist import hypothesis as phase6_hypothesis
 from research_db.persist import measure as phase7_measure
 from research_db.persist import group as phase8_group
+from research_db.persist import analyze as phase9_analyze
+from research_db.persist import regime as phase10_regime
 
 phase4_observe.bind(FixtureStore)
 phase5_state.bind(FixtureStore)
 phase6_hypothesis.bind(FixtureStore)
 phase7_measure.bind(FixtureStore)
 phase8_group.bind(FixtureStore)
+phase9_analyze.bind(FixtureStore)
+phase10_regime.bind(FixtureStore)
 
 
 def open_observation_store(path: str = ":memory:"):
@@ -43,6 +47,18 @@ def open_group_store(path: str = ":memory:"):
     return store
 
 
+def open_analytical_store(path: str = ":memory:"):
+    store = open_group_store(path)
+    store.install_phase9()
+    return store
+
+
+def open_regime_store(path: str = ":memory:"):
+    store = open_analytical_store(path)
+    store.install_phase10()
+    return store
+
+
 __all__ = [
     "FixtureStore",
     "LiveQueryDenied",
@@ -53,4 +69,6 @@ __all__ = [
     "open_hypothesis_store",
     "open_measurement_store",
     "open_group_store",
+    "open_analytical_store",
+    "open_regime_store",
 ]
